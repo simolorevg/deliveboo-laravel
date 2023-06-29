ed21853aaae191b958587ba0989aa15a72d7d8d6
Fedekh <federicocet@gmail.com>
2023-06-29 11:07:05 +0200

    create dashboard in admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h1 class="text-center my-5">Buongiorno <span class="number text-decoration-underline"> {{ Auth::user()->name }}</span></h1>
+    <h1 class="text-center my-5">Buongiorno
+         <span class="number text-decoration-underline"> {{ Auth::user()->name }}</span>
+        </h1>
 @endsection
