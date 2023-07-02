da95149bee3d8cdf7fce113bc1bc057387b2410b
Fedekh <federicocet@gmail.com>
2023-07-02 12:00:32 +0200

    add favicon

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -13,7 +13,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="{{ Vite::asset('resources/img/favicon.png') }}" type="image/x-icon">
+    <link rel="icon" href="{{ Vite::asset('resources/img/favicon.jpg') }}" type="image/x-icon">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
