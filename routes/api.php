ba914ac6d58409f5fed911aec661322956a74cdf
Fedekh <federicocet@gmail.com>
2023-07-05 12:20:36 +0200

    create first Api restaurants and tested in postman

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\DishController;
+use App\Http\Controllers\Api\RestaurantController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('restaurants', [RestaurantController::class, 'index']);
+Route::get('restaurants/{slug}', [RestaurantController::class, 'show']);
+Route::get('dishes', [DishController::class, 'index']);
