5d5344fce542ea2c41f814dd49c74c362aa9d92a
Fedekh <federicocet@gmail.com>
2023-07-05 15:24:53 +0200

    create controller restaurant/dish with paginate

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -10,10 +10,26 @@ class RestaurantController extends Controller
 {
     public function index()
     {
-        $restaurants = Restaurant::all();   
+        $restaurants = Restaurant::paginate(5); //pagina di 5 risultati per pagina
         return response()->json([
             'success' => true,
             'results' => $restaurants
         ]);
+    }
+
+    public function show($slug)
+    {
+        $restaurant = Restaurant::with('categories', 'dishes', 'user')->where('slug', $slug)->first();
+        if ($restaurant) {
+            return response()->json([
+                'success' => true,
+                'results' => $restaurant
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'error' => 'Ristorante non trovato'
+            ])->setStatusCode(404);
+        };
     }
 }
