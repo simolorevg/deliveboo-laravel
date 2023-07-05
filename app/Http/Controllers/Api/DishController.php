1aa7676a901386b1b1bfb271f79a1c07b9f03b82
Fedekh <federicocet@gmail.com>
2023-07-05 12:22:16 +0200

    add api dishes

--- app/Http/Controllers/Api/DishController.php
+++ app/Http/Controllers/Api/DishController.php
@@ -4,8 +4,16 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Dish;
 
 class DishController extends Controller
 {
-    //
+    public function index()
+    {
+        $dishes = Dish::all();   
+        return response()->json([
+            'success' => true,
+            'results' => $dishes
+        ]);
+    }
 }
