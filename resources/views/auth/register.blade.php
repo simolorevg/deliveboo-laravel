0467a2d34c087929099049c754dfac68c20b2d1a
Fedekh <federicocet@gmail.com>
2023-07-06 09:33:55 +0200

    add style register to span is_need

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}: I campi con l'asterisco sono obbligatori</div>
+                <div class="card-header">{{ __('Register') }}: <span class="need is_need">I campi con l'asterisco sono obbligatori</span></div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}" id="registration-form">
