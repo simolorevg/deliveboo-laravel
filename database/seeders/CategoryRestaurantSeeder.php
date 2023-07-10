0a7f3687a4b3e87dccaf34c4117956fd9f4d5fb0
Fedekh <federicocet@gmail.com>
2023-07-10 15:11:21 +0200

    remake CategoryRestaurantSeeder

--- database/seeders/CategoryRestaurantSeeder.php
+++ database/seeders/CategoryRestaurantSeeder.php
@@ -8,20 +8,18 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class CategoryRestaurantTableSeeder extends Seeder
+class CategoryRestaurantSeeder extends Seeder
 {
     public function run()
     {
-        $categories = Category::all();
         $restaurants = Restaurant::all();
-    
+        $categories = Category::all();
+
         foreach ($restaurants as $restaurant) {
-            $randomCategories = $categories->random(rand(1, 3)); // Genera un numero casuale di categorie per ogni ristorante
-    
-            foreach ($randomCategories as $category) {
-                $restaurant->categories()->attach($category);
-            }
+            $categoryCount = rand(1, 4);
+            $selectedCategories = $categories->random($categoryCount);
+
+            $restaurant->categories()->attach($selectedCategories);
         }
     }
-    
 }
--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -20,17 +20,17 @@ class RestaurantSeeder extends Seeder
     public function run()
     {
         $restaurantsCount = 20;
-        $thumbs = [
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmgY-pdaQF0g-enI6o1zIAQeHgnh15_Ia5l1p1XqVv1M0_lr2ooWtcUOHmo7Cgxge4boc&usqp=CAU',
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQW3VxshCiRPmRUNQQXGfSWtzyQ49d5nJOnPjpg1l7AxZ6NcssW9hO-JuNTmiW8gEv160Y&usqp=CAU',
-            'https://www.pinterest.it/testadelia98/logo-per-ristorante/',
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlEYJolVYyiLG0ZqLTRFCgJ2yV4izrL1qfHAJWFeDOjR1Ly39ikysJwQiyyNx9m-hmPec&usqp=CAU',
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdqm6-YaLkgIFXfeXwEcwJikYQAIilxcvd7Csf1nhK5_GLF9L1o6r7GC9ph-aGSlHT_tI&usqp=CAU',
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLoAkjrbdSaoqMlBuFIdRqh5-IechIp7PffYWmj10nwv_mmXbhsFUnMa1Ih356dCdphiY&usqp=CAU0',
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzPL08WYNdCX8zE80YSMEp3DPgw1ANFjdoWn9oVO8Q4g_Nls1q7vkZ6BPzskpG5ObM9OU&usqp=CAU',
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLSI04CzscU1NaqezJWXaHubcMwwBlEYW7wruYG6d_uKUWASWEh1GF2cJzYlGYBtZQvLM&usqp=CAU',
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2nGn31nUM82FfBeuebUTxFnN5VQV2xyRoZZa23AyTWpFeJ0-WvGTyncs73iUDCldMKZI&usqp=CAU'
-        ];
+        // $thumbs = [
+        //     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmgY-pdaQF0g-enI6o1zIAQeHgnh15_Ia5l1p1XqVv1M0_lr2ooWtcUOHmo7Cgxge4boc&usqp=CAU',
+        //     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQW3VxshCiRPmRUNQQXGfSWtzyQ49d5nJOnPjpg1l7AxZ6NcssW9hO-JuNTmiW8gEv160Y&usqp=CAU',
+        //     'https://www.pinterest.it/testadelia98/logo-per-ristorante/',
+        //     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlEYJolVYyiLG0ZqLTRFCgJ2yV4izrL1qfHAJWFeDOjR1Ly39ikysJwQiyyNx9m-hmPec&usqp=CAU',
+        //     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdqm6-YaLkgIFXfeXwEcwJikYQAIilxcvd7Csf1nhK5_GLF9L1o6r7GC9ph-aGSlHT_tI&usqp=CAU',
+        //     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLoAkjrbdSaoqMlBuFIdRqh5-IechIp7PffYWmj10nwv_mmXbhsFUnMa1Ih356dCdphiY&usqp=CAU0',
+        //     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzPL08WYNdCX8zE80YSMEp3DPgw1ANFjdoWn9oVO8Q4g_Nls1q7vkZ6BPzskpG5ObM9OU&usqp=CAU',
+        //     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLSI04CzscU1NaqezJWXaHubcMwwBlEYW7wruYG6d_uKUWASWEh1GF2cJzYlGYBtZQvLM&usqp=CAU',
+        //     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2nGn31nUM82FfBeuebUTxFnN5VQV2xyRoZZa23AyTWpFeJ0-WvGTyncs73iUDCldMKZI&usqp=CAU'
+        // ];
         for ($i = 1; $i <= $restaurantsCount; $i++) {
             $userId = $i;
             DB::table('restaurants')->insert([
@@ -41,7 +41,7 @@ class RestaurantSeeder extends Seeder
                 'address' => 'Indirizzo ' . $i,
                 'phone' => '12' . str_pad($i, 4, '0', STR_PAD_LEFT),
                 'vat_number' => str_pad(rand(0, 99999999999), 11, '0', STR_PAD_LEFT),
-                'thumb' => $thumbs[array_rand($thumbs)],
+                // 'thumb' => $thumbs[array_rand($thumbs)],
                 'closure_day' => 'Chiusura ' . $i,
                 'created_at' => now(),
                 'updated_at' => now()
