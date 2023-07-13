d5199c254f0511013936dba5e056b966a7d21415
simolorevg <simoneviaggi@libero.it>
2023-07-13 12:10:51 +0200

    fixed the problem with the filter

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -1,27 +1,28 @@
 @extends('layouts.admin')
 
 @section('content')
-<div class="wrapper p-5">
+    <div class="wrapper p-5">
 
-    <h2>I tuoi ordini <span class="info">{{ Auth::user()->name }}</span></h2>
+        <h2>I tuoi ordini <span class="info">{{ Auth::user()->name }}</span></h2>
 
-    <table class="table table-dark table-hover table-striped">
-        <thead>
-            <tr class="text-center">
-                <th scope="col">Nome </th>
-                <th scope="col">Cognome </th>
-                <th scope="col">Indirizzo</th>
-                <th scope="col">Telefono</th>
-                <th scope="col">Email</th>
-                <th scope="col">Totale piatti</th>
-                <th scope="col">Totale €</th>
-                <th scope="col">Azioni</th>
-            </tr>
-        </thead>
-        <tbody>
+        <table class="table table-dark table-hover table-striped">
+            <thead>
+                <tr class="text-center">
+                    <th scope="col">Nome </th>
+                    <th scope="col">Cognome </th>
+                    <th scope="col">Indirizzo</th>
+                    <th scope="col">Telefono</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Totale piatti</th>
+                    <th scope="col">Totale €</th>
+                    <th scope="col">Azioni</th>
+                </tr>
+            </thead>
+            <tbody>
 
-            @foreach ($orders as $item)
-                @if ($item->restaurant_id == Auth::user()->id)
+                @foreach ($orders as $item)
+                    {{-- @dd($orders) --}}
+                    {{-- @if ($item->restaurant_id == Auth::user()->id) --}}
                     <tr class="text-center">
                         <td scope="row">{{ $item->guest_name }}</td>
                         <td scope="row">{{ $item->guest_lastname }}</td>
@@ -31,24 +32,24 @@
                         <td scope="row">{{ $item->total_goods }}</td>
                         <td scope="row">{{ $item->total }}</td>
                         <td scope="row" class="d-flex justify-content-center">
-                            <a class="btn btn-primary mx-1"
-                                href="{{ route('admin.orders.show', $item->slug) }}">Visualizza</a>
-                            <form class="d-inline-block" action="{{ route('admin.orders.destroy', $item->slug) }}"
-                                method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger btn-delete"
-                                    data-dish-name="{{ $item->dish_name }}">Elimina</button>
-                            </form>
+                            {{-- <a class="btn btn-primary mx-1"
+                                    href="{{ route('admin.orders.show', $item->slug) }}">Visualizza</a>
+                                <form class="d-inline-block" action="{{ route('admin.orders.destroy', $item->slug) }}"
+                                    method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger btn-delete"
+                                        data-dish-name="{{ $item->dish_name }}">Elimina</button>
+                                </form> --}}
                         </td>
                     </tr>
-                @endif
-            @endforeach
-        </tbody>
-    </table>
-    {{-- <a href="{{ route('admin.stat')}}">Guarda le stat</a> --}}
-    {{-- {{ $orders->links() }} --}}
+                    {{-- @endif --}}
+                @endforeach
+            </tbody>
+        </table>
+        {{-- <a href="{{ route('admin.stat')}}">Guarda le stat</a> --}}
+        {{-- {{ $orders->links() }} --}}
 
-    @include('admin.partials.modal_delete')
-</div>
+        @include('admin.partials.modal_delete')
+    </div>
 @endsection
